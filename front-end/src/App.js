84e2e0e06d0da71d55574545a31019152658f746
Lewis Jones <lewisjones0000@gmail.com>
2021-06-22 15:19:40 +0100

    API - Cors Attempting FIX

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Home from './components/common/home'
-import Converter from './components/common/converter'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/common/home";
+import Converter from "./components/common/converter";
 // import Navbar from './components/common/navbar'
-import { getData } from '../src/lib/api'
+import { getData } from "../src/lib/api";
 
 class App extends React.Component {
-
   async componentDidMount() {
-    const getDataSet = await getData()
-    console.log(getDataSet)
+    const getDataSet = await getData();
+    console.log(getDataSet);
   }
 
-  
   render() {
     return (
       <BrowserRouter>
-      {/* <Navbar>
+        {/* <Navbar>
       </Navbar> */}
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/converter" component={Converter} />
         </Switch>
       </BrowserRouter>
-    )
+    );
   }
 }
 
-export default App
+export default App;
